42592478913b617c939be81c6234c8fcccafc6c3
Ryan Jagger <ryanjagger@gmail.com>
2015-12-16 19:42:40 -0600

    First commit

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,23 @@
+var fs = require('fs');
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+
 // require more modules/folders here!
 
+
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  fs.readFile(__dirname+'/public/index.html', 'utf8', function(error, data){
+    if(error) { console.log(error);}
+
+    //console.log(data);
+    res.writeHead(200);
+    res.write(data);
+
+    res.end();
+  });
+
+  //res.end(archive.paths.list);
+
+
 };
